352f84cdb84c3e4031e162ac94bf0ac7251d298d
PeyaPeyaPeyang <peyang@peya.tokyo>
2025-08-21 23:42:51 +0900

    feat: Injecting isArray

--- src/main/java/tokyo/peya/langjal/vm/engine/injections/InjectorClass.java
+++ src/main/java/tokyo/peya/langjal/vm/engine/injections/InjectorClass.java
@@ -3,6 +3,7 @@ package tokyo.peya.langjal.vm.engine.injections;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.tree.MethodNode;
+import tokyo.peya.langjal.compiler.jvm.AccessAttribute;
 import tokyo.peya.langjal.compiler.jvm.EOpcodes;
 import tokyo.peya.langjal.vm.VMSystemClassLoader;
 import tokyo.peya.langjal.vm.engine.VMClass;
@@ -10,10 +11,10 @@ import tokyo.peya.langjal.vm.engine.threading.VMThread;
 import tokyo.peya.langjal.vm.references.ClassReference;
 import tokyo.peya.langjal.vm.values.VMBoolean;
 import tokyo.peya.langjal.vm.values.VMObject;
-import tokyo.peya.langjal.vm.values.VMStringCreator;
 import tokyo.peya.langjal.vm.values.VMType;
 import tokyo.peya.langjal.vm.values.VMValue;
 import tokyo.peya.langjal.vm.values.metaobjects.VMClassObject;
+import tokyo.peya.langjal.vm.values.metaobjects.VMStringObject;
 
 public class InjectorClass implements Injector
 {
@@ -37,8 +38,7 @@ public class InjectorClass implements Injector
                         "()V",
                         null,
                         null
-                )
-                )
+                ))
                 {
                     @Override
                     @Nullable VMValue invoke(@NotNull VMThread thread, @Nullable VMClass caller,
@@ -57,8 +57,7 @@ public class InjectorClass implements Injector
                         "(Ljava/lang/Class;)Z",
                         null,
                         null
-                )
-                )
+                ))
                 {
                     @Override VMValue invoke(@NotNull VMThread thread, @Nullable VMClass caller,
                                              @Nullable VMObject instance, @NotNull VMValue[] args)
@@ -71,24 +70,21 @@ public class InjectorClass implements Injector
                 cl,
                 new InjectedMethod(
                         clazz, new MethodNode(
-                         EOpcodes.ACC_STATIC | EOpcodes.ACC_NATIVE,
+                        EOpcodes.ACC_STATIC | EOpcodes.ACC_NATIVE,
                         "getPrimitiveClass",
                         "(Ljava/lang/String;)Ljava/lang/Class;",
                         null,
                         null
-                )
-                )
+                ))
                 {
                     @Override VMValue invoke(@NotNull VMThread thread, @Nullable VMClass caller,
                                              @Nullable VMObject instance, @NotNull VMValue[] args)
                     {
-                        VMValue arg = args[0];
-                        if (!(arg instanceof VMObject object))
-                            throw new IllegalArgumentException("Expected a VMObject as argument");
-                        String str = VMStringCreator.getString(object);
+                        VMStringObject string = (VMStringObject) args[0];
+                        String str = string.getString();
                         return VMType.getPrimitiveType(str)
                                      .getLinkedClass()
-                                     .getClassObject(thread.getVm().getClassLoader());
+                                     .getClassObject();
                     }
                 }
         );
@@ -101,8 +97,7 @@ public class InjectorClass implements Injector
                         "()Z",
                         null,
                         null
-                )
-                )
+                ))
                 {
                     @Override VMValue invoke(@NotNull VMThread thread, @Nullable VMClass caller,
                                              @Nullable VMObject instance, @NotNull VMValue[] args)
@@ -110,6 +105,75 @@ public class InjectorClass implements Injector
                         assert instance != null: "Instance must be a VMObject";
                         VMClassObject instanceClass = (VMClassObject) instance;
                         return VMBoolean.of(instanceClass.getTypeOf().isPrimitive());
+                    }
+                }
+        );
+        clazz.injectMethod(
+                cl,
+                new InjectedMethod(
+                        clazz, new MethodNode(
+                        EOpcodes.ACC_PRIVATE | EOpcodes.ACC_STATIC | EOpcodes.ACC_NATIVE,
+                        "forName0",
+                        "(Ljava/lang/String;ZLjava/lang/ClassLoader;Ljava/lang/Class;)Ljava/lang/Class;",
+                        null,
+                        null
+                ))
+                {
+                    @Override VMValue invoke(@NotNull VMThread thread, @Nullable VMClass caller,
+                                             @Nullable VMObject instance, @NotNull VMValue[] args)
+                    {
+                        VMStringObject classNameObject = (VMStringObject) args[0];
+                        boolean initialize = ((VMBoolean) args[1]).asBoolean();
+
+                        ClassReference classReference = ClassReference.of(classNameObject.getString());
+                        VMClass loadedClass = cl.findClass(classReference);
+                        if (initialize)
+                            loadedClass.initialise(thread);
+
+                        return loadedClass.getClassObject();
+                    }
+                }
+        );
+        clazz.injectMethod(
+                cl,
+                new InjectedMethod(
+                        clazz, new MethodNode(
+                        EOpcodes.ACC_PUBLIC | EOpcodes.ACC_NATIVE,
+                        "isInterface",
+                        "()Z",
+                        null,
+                        null
+                ))
+                {
+                    @Override VMValue invoke(@NotNull VMThread thread, @Nullable VMClass caller,
+                                             @Nullable VMObject instance, @NotNull VMValue[] args)
+                    {
+                        VMClassObject instanceClass = (VMClassObject) instance;
+                        assert instanceClass != null: "Instance must be a VMClassObject";
+
+                        return VMBoolean.of(instanceClass.getRepresentingClass().getAccessAttributes().has(AccessAttribute.INTERFACE));
+                    }
+                }
+        );
+        clazz.injectMethod(
+                cl,
+                new InjectedMethod(
+                        clazz, new MethodNode(
+                        EOpcodes.ACC_PUBLIC | EOpcodes.ACC_NATIVE,
+                        "isArray",
+                        "()Z",
+                        null,
+                        null
+                ))
+                {
+                    @Override VMValue invoke(@NotNull VMThread thread, @Nullable VMClass caller,
+                                             @Nullable VMObject instance, @NotNull VMValue[] args)
+                    {
+                        VMClassObject instanceClass = (VMClassObject) instance;
+                        assert instanceClass != null: "Instance must be a VMClassObject";
+
+                        VMClass representingClass = instanceClass.getRepresentingClass();
+                        return VMBoolean.of(representingClass.getArrayDimensions() > 0);
                     }
                 }
         );

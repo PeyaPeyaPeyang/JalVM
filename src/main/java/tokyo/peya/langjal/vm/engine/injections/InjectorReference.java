7623654f393d3a94a41f9a714bc46fc411511958
PeyaPeyaPeyang <peyang@peya.tokyo>
2025-09-28 02:35:47 +0900

    feat: refersTo0

--- src/main/java/tokyo/peya/langjal/vm/engine/injections/InjectorReference.java
+++ src/main/java/tokyo/peya/langjal/vm/engine/injections/InjectorReference.java
@@ -7,9 +7,13 @@ import tokyo.peya.langjal.compiler.jvm.EOpcodes;
 import tokyo.peya.langjal.vm.VMSystemClassLoader;
 import tokyo.peya.langjal.vm.engine.VMClass;
 import tokyo.peya.langjal.vm.engine.VMFrame;
+import tokyo.peya.langjal.vm.engine.members.VMField;
 import tokyo.peya.langjal.vm.engine.threading.VMThreadState;
 import tokyo.peya.langjal.vm.references.ClassReference;
+import tokyo.peya.langjal.vm.values.VMBoolean;
+import tokyo.peya.langjal.vm.values.VMNull;
 import tokyo.peya.langjal.vm.values.VMObject;
+import tokyo.peya.langjal.vm.values.VMReferenceValue;
 import tokyo.peya.langjal.vm.values.VMValue;
 
 public class InjectorReference implements Injector
@@ -42,6 +46,38 @@ public class InjectorReference implements Injector
                     {
                         frame.getThread().setState(VMThreadState.WAITING_INDEFINITELY);
                         return null;
+                    }
+                }
+        );
+        clazz.injectMethod(
+                new InjectedMethod(
+                        clazz, new MethodNode(
+                        EOpcodes.ACC_PRIVATE | EOpcodes.ACC_NATIVE,
+                        "refersTo0",
+                        "(Ljava/lang/Object;)Z",
+                        null,
+                        null
+                )
+                )
+                {
+                    @Override
+                    protected VMValue invoke(@NotNull VMFrame frame, @Nullable VMClass caller,
+                                             @Nullable VMObject instance, @NotNull VMValue[] args)
+                    {
+                        assert instance != null;
+                        VMField referentField = clazz.findField("referent");
+                        VMReferenceValue referent = (VMReferenceValue) instance.getField(referentField);
+
+                        boolean result;
+                        VMValue other = args[0];
+                        if (other instanceof VMNull<?>)
+                            result = referent instanceof VMNull<?>;
+                        else if (referent instanceof VMNull<?>)
+                            result = false;
+                        else
+                            result = referent.equals(other);
+
+                        return VMBoolean.of(frame, result);
                     }
                 }
         );
